4f2dc194ccdeb1e16ce371b4b18179d71169c7dc
Andrew <andrewdblum@gmail.com>
2018-06-15 20:30:51 -0700

    refactor

--- test/controllers/values_controller_test.rb
+++
@@ -1,9 +0,0 @@
-require 'test_helper'
-
-class ValuesControllerTest < ActionDispatch::IntegrationTest
-  test "should get create" do
-    get values_create_url
-    assert_response :success
-  end
-
-end
