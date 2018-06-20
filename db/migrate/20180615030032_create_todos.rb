08dbfe1fd0b7219ee6b2db68d8d2b19d34a966eb
Andrew <andrewdblum@gmail.com>
2018-06-19 18:40:02 -0700

    todo migration

--- db/migrate/20180615030032_create_todos.rb
+++ db/migrate/20180615030032_create_todos.rb
@@ -1,6 +1,10 @@
 class CreateTodos < ActiveRecord::Migration[5.2]
   def change
     create_table :todos do |t|
+      t.string :title, null: false
+      t.text :body
+      t.integer :user_id, null: false
+      t.boolean :done, null: false
 
       t.timestamps
     end
